bd0f4c1aa5d5c55598d313767c86b8a3b2298246
sandushi <it23202740@my.sliit.lk>
2026-06-04 22:51:08 +0530

    authentication and login backend

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import Admin from "../models/Admin.js";
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token =
+        req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET
+      );
+
+      req.admin = await Admin.findById(
+        decoded.id
+      ).select("-password");
+
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        message: "Not authorized",
+      });
+    }
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      message: "No token",
+    });
+  }
+};
+
+export default protect;
